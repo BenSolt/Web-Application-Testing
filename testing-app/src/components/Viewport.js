9565561ac4771349f0f91d312328c8b86811da16
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-12-05 14:29:54 -0500

    adding components

---
+++ testing-app/src/components/Viewport.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Viewport = props => {
+    return (
+        <div>
+            <h1>BaseBall App</h1>
+            <h2>Balls: {props.displayBalls}</h2>
+            <h2>Strikes: {props.displayStrikes}</h2>
+            
+        </div>
+    )
+}
+
+export default Viewport;
